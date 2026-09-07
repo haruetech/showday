be70abb1cef257195cf11066d00b4bed1192fff7
haruetech <haruetech@gmail.com>
2026-09-07 23:32:27 +0900

    Delete components directory

--- components/Header.tsx
+++
@@ -1,5 +0,0 @@
-import Link from 'next/link';
-export default function Header(){
- const nav=[['공연','/shows'],['아티스트','/artists'],['공연장','/venues'],['AROUND','/around'],['50+','/50plus'],['ARENA NOW','/arena']];
- return <header className="header"><Link className="logo" href="/">SHOWDAY<span>•</span></Link><nav>{nav.map(([n,h])=><Link key={h} href={h}>{n}</Link>)}</nav><Link className="my" href="/my">MY SHOWDAY</Link></header>
-}
